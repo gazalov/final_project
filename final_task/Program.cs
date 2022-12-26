b85b6d77f2f86d442710ed24c076eb8a89c12e4f
a.gazalov <gazalov@list.ru>
2022-12-26 15:46:01 +0300

    Program for array initialization from other array using condition on number of chars in the element

--- final_task/Program.cs
+++ final_task/Program.cs
@@ -1,1 +1,43 @@
-﻿
+﻿Console.Clear();
+
+string[] StringArraySelect(string[] arrayToSelect, int countChar)
+{
+    int countElements = 0;
+    for (int i = 0; i < arrayToSelect.Length; i++)
+    {
+        if (arrayToSelect[i].Length <= countChar)
+        {
+            countElements++;
+        }
+    }
+
+    string[] newArray = new string[countElements];
+
+    int newArrayCount = 0;
+    for (int i = 0; i < arrayToSelect.Length; i++)
+    {
+        if (arrayToSelect[i].Length <= countChar)
+        {
+            newArray[newArrayCount] = arrayToSelect[i];
+            newArrayCount++;
+        }
+    }
+    return newArray;
+}
+
+void PrintArray(string[] arrayToPrint)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < arrayToPrint.Length; i++)
+    {
+        Console.Write($"{arrayToPrint[i]} ");
+    }
+    Console.Write("]");
+}
+
+int countChar = 3;
+string[] array = { "hello", "2", "world", ":-" };
+PrintArray(array);
+string[] newArray = StringArraySelect(array, countChar);
+Console.WriteLine();
+PrintArray(newArray);
